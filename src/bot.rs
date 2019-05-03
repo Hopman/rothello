90c55f494aeed2547baff3261cd2d3b47584713c
Hopman <joost@jhopmans.nl>
2019-05-03 15:44:01 +0200

    multithreading works

--- src/bot.rs
+++ src/bot.rs
@@ -1,5 +1,8 @@
 use crate::*;
 
+use std::sync::{Arc, Mutex};
+use std::thread;
+
 #[derive(Clone, Debug)]
 struct Node {
     mvval: usize,
@@ -14,24 +17,51 @@ impl Node {
 }
 
 pub fn bot_turn(mut board: &mut Board, color: usize, depth: usize) -> usize {
-    let node = Node {
+
+    let valid_moves = get_valid_moves(&board, color);
+
+    let mut handles = Vec::new();
+
+    let mut top_node = Node {
         mvval: usize::max_value(),
         score: 0,
         children: Vec::new(),
     };
-    let result = bot_rec(&mut board, color, depth, node);
+
+    for i in valid_moves {
+        println!("I -> {:?}", i);
+        let mut mv_board = board.clone();
+        mv_board.execute_move(&i, color);
+        let handle = thread::spawn(move || {
+            let t_node = Node {
+                mvval: i.0,
+                score: 0,
+                children: Vec::new(),
+            };
+            let t_color = color;
+            let result_node = bot_rec(&mv_board, t_color, 1, t_node);
+            return result_node
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        let res_nod = handle.join().unwrap();
+        top_node.add_child(res_nod);
+    }
     let mut bot_move = 0;
     let mut bot_max_score = isize::min_value();
-    for c in &result.children {
+    for c in &top_node.children {
         if c.score > bot_max_score {
             bot_max_score = c.score;
             bot_move = c.mvval;
         }
     }
+
     return bot_move
 }
 
-fn bot_rec(mut board: &mut Board, color: usize, depth: usize, mut node: Node) -> Node {
+fn bot_rec(mut board: &Board, color: usize, depth: usize, mut node: Node) -> Node {
     if depth > 8 {
         return node;
     }
